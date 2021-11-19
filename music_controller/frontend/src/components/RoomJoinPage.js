6313da091e303b99f9fabd7395b836c6407a7c19
mohamad9999 <mohtashamimohamad99@gmail.com>
2021-11-19 23:24:42 +0300

    join room

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,43 @@
 import React, {Component} from "react";
+import { Button, TextField, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            roomCode: "",
+            error: ""
+        }
     }
 
     render(){
-        return<p>This is Room join page</p>
+        return(
+            <Grid container spacing={1}  align="center">
+                <Grid item xs={12} >
+                    <Typography component="h4" variant="h4">Join a Room</Typography>
+                </Grid>
+                <Grid item xs={12} >
+                    <TextField 
+                        error={this.state.error}
+                        label="code"
+                        placeholder="Enter a Room code"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary">
+                        Enter the Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="secondary" to="/" component={Link}>
+                        Back
+                    </Button>
+                </Grid>
+            </Grid>
+        );
     }
 }
